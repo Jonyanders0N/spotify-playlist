b90fd21461974d5baf660ff44bb1e241f0725a28
Jonyanderson <sonyanderson19@gmail.com>
2025-04-19 18:05:43 +0100

    Install Angular Material 19

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
     selector: 'app-search',
-    imports: [],
+    imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatButtonModule],
     templateUrl: './search.component.html',
     styleUrl: './search.component.scss'
 })
 export class SearchComponent {
+    artistFormControl = new FormControl('', Validators.required)
 
+    searchArtist(){
+        
+    }
 }
